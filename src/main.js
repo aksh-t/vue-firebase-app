c3ddf75da460a25eef5bef4985cd696081f2d8fe
aksh-t <tanno.ao@gmail.com>
2019-07-28 18:58:14 +0900

    firebase config

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+var firebaseConfig = {
+  apiKey: "AIzaSyD-mktsdvfvBzbR7JYU2z_A1qc7FACJMkw",
+  authDomain: "hotate-6f260.firebaseapp.com",
+  databaseURL: "https://hotate-6f260.firebaseio.com",
+  projectId: "hotate-6f260",
+  storageBucket: "hotate-6f260.appspot.com",
+  messagingSenderId: "1039979808855",
+  appId: "1:1039979808855:web:3819386dd736a878"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   render: h => h(App),
