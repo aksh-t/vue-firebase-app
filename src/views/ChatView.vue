e9178dba14919620e7f9fa3fa2b7211b9dd61f90
aksh-t <tanno.ao@gmail.com>
2019-08-03 00:12:11 +0900

    Chat用のコンポーネントを作成

---
+++ src/views/ChatView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="chatView">
+    <Chat/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Chat from "@/components/Chat.vue";
+
+export default {
+  name: "chatView",
+  components: {
+    Chat
+  }
+};
+</script>
